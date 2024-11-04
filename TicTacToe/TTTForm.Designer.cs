029d3d7bfd3065bd12285ae75601b55e0a901f06
Пользователь <@DESKTOP-C9VCH6S>
2024-11-04 08:04:38 +0700

    Добавьте файлы проекта.

---
+++ TicTacToe/TTTForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TicTacToe
+{
+    partial class TTTForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.C3);
+            this.panel1.Controls.Add(this.C2);
+            this.panel1.Controls.Add(this.C1);
+            this.panel1.Controls.Add(this.B3);
+            this.panel1.Controls.Add(this.B2);
+            this.panel1.Controls.Add(this.B1);
+            this.panel1.Controls.Add(this.A3);
+            this.panel1.Controls.Add(this.A2);
+            this.panel1.Controls.Add(this.A1);
+            this.panel1.Location = new System.Drawing.Point(130, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.Black;
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C3.Location = new System.Drawing.Point(210, 210);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(90, 90);
+            this.C3.TabIndex = 26;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.Black;
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C2.Location = new System.Drawing.Point(106, 210);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(90, 90);
+            this.C2.TabIndex = 25;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.Black;
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C1.Location = new System.Drawing.Point(0, 210);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(90, 90);
+            this.C1.TabIndex = 24;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.Black;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B3.Location = new System.Drawing.Point(210, 105);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(90, 90);
+            this.B3.TabIndex = 23;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.Black;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B2.Location = new System.Drawing.Point(106, 105);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(90, 90);
+            this.B2.TabIndex = 22;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.Black;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B1.Location = new System.Drawing.Point(0, 105);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(90, 90);
+            this.B1.TabIndex = 21;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.Black;
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A3.Location = new System.Drawing.Point(210, 0);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(90, 90);
+            this.A3.TabIndex = 20;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.Black;
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A2.Location = new System.Drawing.Point(106, 0);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(90, 90);
+            this.A2.TabIndex = 19;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.Black;
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A1.Location = new System.Drawing.Point(0, 0);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(90, 90);
+            this.A1.TabIndex = 18;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // TTTForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(577, 439);
+            this.Controls.Add(this.panel1);
+            this.Name = "TTTForm";
+            this.Text = "TTTForm";
+            this.Load += new System.EventHandler(this.TTTForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A1;
+    }
+}
---
+++ TicTacToe/TTTForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TTTForm : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+
+        public TTTForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TTTForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            if (turn_count == 9) return;
+
+            Button b = (Button)sender;
+
+            if (b.Text != "") return;
+
+            if (turn)
+            {
+                b.ForeColor = Color.OrangeRed;
+                b.Text = "X";
+            }
+            else
+            {
+                b.ForeColor = Color.LightSkyBlue;
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            turn_count++;
+
+            checkForWin();
+        }
+
+        private void checkForWin ()
+        {
+            bool there_is_a_winner = false;
+
+            if (A1.Text == A2.Text && A2.Text == A3.Text && A1.Text != "")
+                there_is_a_winner = true;
+            else if (B1.Text == B2.Text && B2.Text == B3.Text && B1.Text != "")
+                there_is_a_winner = true;
+            else if (C1.Text == C2.Text && C2.Text == C3.Text && C1.Text != "")
+                there_is_a_winner = true;
+
+            else if (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != "")
+                there_is_a_winner = true;
+            else if (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "")
+                there_is_a_winner = true;
+            else if (A3.Text == B3.Text && B3.Text == C3.Text && A3.Text != "")
+                there_is_a_winner = true;
+
+            else if (A1.Text == B2.Text && B2.Text == C3.Text && A1.Text != "")
+                there_is_a_winner = true;
+            else if (A3.Text == B2.Text && B2.Text == C1.Text && A3.Text != "")
+                there_is_a_winner = true;
+
+            if (there_is_a_winner)
+            {
+                turn_count = 9;
+                String winner = "";
+                if (!turn)
+                    winner = "X";
+                else
+                    winner = "O";
+
+                MessageBox.Show(winner + " Wins!", "Grats!!!");
+            }
+            else
+                if (turn_count == 9)
+            {
+                MessageBox.Show("Draw!", "Good Game!!!");
+
+            }
+
+
+        }
+    }
+}
